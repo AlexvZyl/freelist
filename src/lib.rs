1fa9119f605fce2d10d3b1376a607f41d095f7c8
GitHub Action <alex-vzyl@users.noreply.github.com>
2022-10-11 18:54:03 +0000

    Automated formatting

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,30 @@ use std::vec::Vec;
 
 /// A cache coherent, heap allocated collection.
 pub struct Freelist<T> {
-
     type_size: usize,
     data: Vec<T>,
-    first_free_block: Option<i32>
-
+    first_free_block: Option<i32>,
 }
 
 /// Used to describe an open block in freelist::Freelist<T>.
 /// A block can consist of many blocks, if they are contiguous.
 struct Block {
-
     /// Describes the size of the block by how many of <T> it can fit.
     /// (A block can consist of many contiguous blocks)
     size: i32,
     /// Index to the next free block in the freelist.
-    next_block: Option<i32>
-
+    next_block: Option<i32>,
 }
 
 // Freelist implementations.
 impl<T> Freelist<T> {
-
     /// Create a new empty freelist.
     pub fn new() -> Self {
         Freelist {
             type_size: mem::size_of::<T>(),
             data: Vec::new(),
-            first_free_block: Some::<i32>(0)
-        } 
+            first_free_block: Some::<i32>(0),
+        }
     }
 
     pub fn type_size(&self) -> usize {
@@ -41,5 +36,4 @@ impl<T> Freelist<T> {
     fn has_free_block(&self) -> bool {
         self.first_free_block != None
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use freelist::Freelist;
 
 fn main() {
-
-    let fl = Freelist::<i32>::new(); 
+    let fl = Freelist::<i32>::new();
     print!("{}", fl.type_size());
-
-} 
+}
