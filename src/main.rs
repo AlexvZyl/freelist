fab97f8f30b06061f65b23899b2ffc71e540fc43
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-12-31 15:46:23 +0200

    Minor

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,12 @@ impl Drop for Test
 {
     fn drop(&mut self)
     {
-        print!("Ddropping!");
+        print!("Dropping!");
     }
 }
 
 fn main()
 {
-    println!("{}", std::mem::size_of::<Option<i32>>());
     let fl = Freelist::<Test>::new();
-    print!("{}", fl.type_size_bytes());
+    print!("{}", fl.capacity_bytes());
 }
